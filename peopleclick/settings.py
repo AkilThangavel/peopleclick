536e97e5ef05b621087bf6191f572a4067d3d41c
akilthangavel1 <akilthangavel@gmail.com>
2024-10-12 14:50:51 +0530

    corrections

--- peopleclick/settings.py
+++ peopleclick/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&i*oul070y#yg)ml^38hed=3y47em1wp!a5%j3wvdz@=5&kjuz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 
